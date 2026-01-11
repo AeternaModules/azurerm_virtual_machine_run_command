76c1f0c04bcfc142373770ed8b8ba71b8ea95c5c
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:19:36 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "virtual_machine_run_commands" {
+  description = "All virtual_machine_run_command resources"
+  value       = azurerm_virtual_machine_run_command.virtual_machine_run_commands
+}
+
---
+++ variables.tf
@@ -0,0 +1,71 @@
+variable "virtual_machine_run_commands" {
+  description = <<EOT
+Map of virtual_machine_run_commands, attributes below
+Required:
+    - location
+    - name
+    - virtual_machine_id
+    - source (block):
+        - command_id (optional)
+        - script (optional)
+        - script_uri (optional)
+        - script_uri_managed_identity (optional, block):
+            - client_id (optional)
+            - object_id (optional)
+Optional:
+    - error_blob_uri
+    - output_blob_uri
+    - run_as_password
+    - run_as_user
+    - tags
+    - error_blob_managed_identity (block):
+        - client_id (optional)
+        - object_id (optional)
+    - output_blob_managed_identity (block):
+        - client_id (optional)
+        - object_id (optional)
+    - parameter (block):
+        - name (required)
+        - value (required)
+    - protected_parameter (block):
+        - name (required)
+        - value (required)
+EOT
+
+  type = map(object({
+    location           = string
+    name               = string
+    virtual_machine_id = string
+    error_blob_uri     = optional(string)
+    output_blob_uri    = optional(string)
+    run_as_password    = optional(string)
+    run_as_user        = optional(string)
+    tags               = optional(map(string))
+    source = object({
+      command_id = optional(string)
+      script     = optional(string)
+      script_uri = optional(string)
+      script_uri_managed_identity = optional(object({
+        client_id = optional(string)
+        object_id = optional(string)
+      }))
+    })
+    error_blob_managed_identity = optional(object({
+      client_id = optional(string)
+      object_id = optional(string)
+    }))
+    output_blob_managed_identity = optional(object({
+      client_id = optional(string)
+      object_id = optional(string)
+    }))
+    parameter = optional(object({
+      name  = string
+      value = string
+    }))
+    protected_parameter = optional(object({
+      name  = string
+      value = string
+    }))
+  }))
+}
+
